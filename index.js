551327576f5dd1903e472189487682c0dc487c8f
0xmuralik <krishna192reddy@gmail.com>
2025-02-28 15:54:25 -0700

    add eth storage api endpoints

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { mintCapacityCredits } from './mint_credits.js';
 import { requestCapacity } from './delegateCompute.js';
+import cors from 'cors';
+import axios from 'axios';
+import pkg from './ethstorage_helper.cjs';
+const { createFlatDirectory, uploadData } = pkg;
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+let contractAddress = "";
+
+// Enable CORS for all routes
+app.use(cors());
+
 // Middleware to parse JSON bodies
 app.use(express.json());
+
+// init ethStorage
+ contractAddress = await createFlatDirectory();
 
 // Define a simple API route
 app.get('/mint-capacity-credits', async (req, res) => {
@@ -21,7 +33,7 @@ app.get('/mint-capacity-credits', async (req, res) => {
     }
 });
 
-app.get('/delegate-capacity', async (req, res) => {
+app.post('/delegate-capacity', async (req, res) => {
     const data = req.body;
     const userAddress = data["userAddress"];
     try{
@@ -30,6 +42,33 @@ app.get('/delegate-capacity', async (req, res) => {
     }catch(error){
         console.error('Error delegating capacity credits:', error);
         res.status(500).json({ error: 'Internal Server Error, '+error });
+    }
+});
+
+app.post("/upload-data", async (req, res) => {
+    const data = req.body;
+    const key = data["key"];
+    const content = data["content"];
+    try{
+        await uploadData(key, content, contractAddress);
+        res.json({ success: true });
+    }catch(error){
+        console.error('Error uploading data:', error);
+        res.status(500).json({ error: 'Internal Server Error, '+error });
+    }
+});
+
+app.get("/get-data/:key", async (req, res) => {
+    console.log("contractAddress: ", contractAddress);
+    const key = req.params.key;
+    const url =  `https://${contractAddress}.3337.w3link.io/${key}`;
+    console.log("URL: ", url);
+    try{
+        const response = await axios.get(url);
+        res.json({ data: response.data });
+    } catch(error){
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Internal Server Error, '+error+" URL: "+url });
     }
 });
 
